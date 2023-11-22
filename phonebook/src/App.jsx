84e98eeac430d20f49dff09c68376586d874af96
patriciomackinlay <141454324+patriciomackinlay@users.noreply.github.com>
2023-11-22 17:06:55 -0300

    Complete 2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -3,16 +3,19 @@ import Contact from "./components/Contact"
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "040-1234567",
-   }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] =useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [newFilter, setNewFilter] = useState("")
+
+  const filteredPhonebook = persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()))
 
   const addContact = (e) => {
     e.preventDefault()
-    
     let found = persons.find(person => person.name === newName)
     if (found === undefined) {
       const newPerson = {
@@ -21,6 +24,7 @@ const App = () => {
       }
       setPersons(persons.concat(newPerson))
       setNewName("")
+      setNewNumber("")
     }
     else {
       alert(`${newName} has already been added to the phonebook`)
@@ -36,9 +40,19 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleFilterChange = (e) => {
+    setNewFilter(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter: <input
+          value={newFilter}
+          onChange={handleFilterChange} />
+        </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input
@@ -58,7 +72,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
     <ul>
-      {persons.map(person =>
+      {filteredPhonebook.map(person =>
         <Contact name={person.name} key={person.name} number={person.number} />
         )}
     </ul>
